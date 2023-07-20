6461c43448b059b38d5c2121474f8426282023b3
Guido <guidopasciucco@gmail.com>
2023-07-20 10:43:36 -0300

    configurando el entorno

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function App () {
+  return (
+      <div className="App">
+        <h1>Google Traslate</h1>
+      </div>
+  )
+}
+
+export default App
