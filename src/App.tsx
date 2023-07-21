9a90f8797d66e974211d3b31498f810e4a7b9b9f
Guido <guidopasciucco@gmail.com>
2023-07-21 07:30:00 -0300

    reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,82 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useReducer } from 'react'
 import './App.css'
+import { type State, type Action } from './types'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+// 1. Create a initalState
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+// 2. Create a reducer
+function reducer (state: State, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    }
+  }
+
+  return state
+}
+
 function App () {
+  // 3. Usar el hook useReducer
+  const [{
+    fromLanguage
+    /* toLanguage,
+    fromText,
+    result,
+    loading */
+  }, dispatch] = useReducer(reducer, initialState)
+
+  console.log({ fromLanguage })
+
   return (
       <div className="App">
         <h1>Google Traslate</h1>
+        <button onClick={() => {
+          dispatch({ type: 'SET_FROM_LANGUAGE', payload: 'es' })
+        }}>Cambiar a Español</button>
+        {fromLanguage}
       </div>
   )
 }
